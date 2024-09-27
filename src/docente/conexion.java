79a7f5b8bc31ad6b1d2f517a791b5718015f0fca
EmiranP5 <emirandap5@miumg.edu.gt>
2024-09-26 22:13:42 -0600

    .

--- src/docente/conexion.java
+++ src/docente/conexion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package docente;
 
 import java.awt.HeadlessException;
@@ -10,10 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author felix
- */
+
 public class conexion {
 
     public static com.sun.jdi.connect.spi.Connection getConnection() {
